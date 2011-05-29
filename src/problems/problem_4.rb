030ef718c6e9326f3e3186dd5973d7987d3ac3c2
DavidBasarab <dbasarab617@gmail.com>
2011-05-29 12:58:09 -0400

    Refactored problem 4 to clean up the code.  Now moving on to problem 5.

--- src/problems/problem_4.rb
+++ src/problems/problem_4.rb
@@ -9,19 +9,58 @@ class Problem4 < EulerProblem
   end
 
   def answer
-    dromes = Array.new
-    first_digit = stop_number
-    while first_digit > start_number do
-      second_digit = stop_number
-      while second_digit > start_number do
-        product = first_digit * second_digit
-        dromes << product if product.palindromic?
-        second_digit = second_digit - 1
-      end
-      first_digit = first_digit - 1
+    @dromics = Array.new
+    initialize_first_digit
+    get_all_palindrome_numbers
+    get_highest_palindromic_number
+  end
+
+  def get_highest_palindromic_number
+    @dromics = @dromics.sort
+    @dromics.last
+  end
+
+  def get_all_palindrome_numbers
+    while first_digit_valid do
+      loop_through_possible_second_digits_and_add_to_array
+      decrement_first_digit
     end
-    dromes = dromes.sort
-    dromes.last
+  end
+
+  def initialize_first_digit
+    @first_digit = stop_number
+  end
+
+  def first_digit_valid
+    @first_digit > start_number
+  end
+
+  def decrement_first_digit
+     @first_digit = @first_digit - 1
+  end
+
+  def loop_through_possible_second_digits_and_add_to_array
+     initialize_second_digit
+      while second_digit_valid do
+        add_product_to_array_if_palindromic (@first_digit * @second_digit)
+        decrement_second_digit
+      end
+  end
+
+  def initialize_second_digit
+    @second_digit = stop_number
+  end
+
+  def second_digit_valid
+    @second_digit > start_number
+  end
+
+  def add_product_to_array_if_palindromic(product)
+    @dromics << product if product.palindromic?
+  end
+
+  def decrement_second_digit
+    @second_digit = @second_digit - 1
   end
 
   def stop_number
