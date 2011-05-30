8030f53414c2d729c964be9ef248321ee23a4234
DavidBasarab <dbasarab617@gmail.com>
2011-05-29 22:40:58 -0400

    In the process of solving problem 5.

--- src/problems/problem_5.rb
+++ src/problems/problem_5.rb
@@ -1,7 +1,9 @@
 class Problem5 < EulerProblem
+  attr_accessor :top_range
 
   def description
-
+      "2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?"
   end
 
   def answer
--- src/spec/problem_5_spec.rb
+++ src/spec/problem_5_spec.rb
@@ -11,6 +11,12 @@ describe Problem5 do
     it "should respond to problem methods" do
       @problem.should respond_to(:description)
       @problem.should respond_to(:answer)
+      @problem.should respond_to(:top_range)
+    end
+
+    it "will find the smallest numbers divided from 1 to 10 without a remainder" do
+      @problem.top_range = 10
+      @problem.answer.should == 2520
     end
 
   end
